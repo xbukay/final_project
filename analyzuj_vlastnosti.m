3d933a5bc59e5c632759711854bb520175edadd8
xgomory <xgomory@stuba.sk>
2020-12-08 15:49:31 +0100

    hranica stability

--- analyzuj_vlastnosti.m
+++ analyzuj_vlastnosti.m
@@ -11,13 +11,17 @@ function analyzuj_vlastnosti(cit, men)
     korene = roots(men);
     P = 0;
     for i=1:length(korene)
-        if korene(i) >= 0
+        if korene(i) > 0
             P = 1;
+        elseif korene(i)==0
+            P = 2;
         end
     end
     
     if P == 0
         disp('System je stabilny');
+    elseif P==2
+        disp('System je na hranici stability')
     else
         disp('System nie je stabilny!');
     end
--- testuj.m
+++ testuj.m
@@ -1,2 +1,3 @@
  analyzuj_vlastnosti([1],[-1 -1]);
+ analyzuj_vlastnosti([-1],[1 0 0]);
  analyzuj_vlastnosti([1 1],[2 -1]);
